3892f606071cb87ec57d672b528365676c799104
rajnish-baxture <srajnish235.rs@gmail.com>
2024-10-18 14:21:05 +0530

    added update for invoice

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Form from "@/app/ui/invoices/create-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
 
-const page = () => {
-  return <div>page</div>;
-};
+export default async function Page() {
+  const customers = await fetchCustomers();
 
-export default page;
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Create Invoice",
+            href: "/dashboard/invoices/create",
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
